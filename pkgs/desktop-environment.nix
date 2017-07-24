0e20e40e005f8d3d07c69e7c26787a2a3dedea3d
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-07-24 16:25:28 +0200

    Add revs and sha256s of environment pkgs

--- pkgs/desktop-environment.nix
+++ pkgs/desktop-environment.nix
@@ -8,8 +8,8 @@ import ./mkEnvironment.nix {
 
   ];
 
-  sha256 = changeme;
-  rev = changeme;
+  sha256 = "402e9ce4815330d6c731b937315d986e";
+  rev = "159e0a7c62729270f7e1aeb9565873473a2712a2";
 
   description = "A local machine user's environment."
 }
--- pkgs/remote-environment.nix
+++ pkgs/remote-environment.nix
@@ -8,8 +8,8 @@ import ./mkEnvironment.nix {
 
   ];
 
-  sha256 = changeme;
-  rev = changeme;
+  sha256 = "3c8fb3096c0e418abc0c5e049d7d6293";
+  rev = "ab8e5d3ff082060a8c42003c16384345fa9e9273";
 
   description = "A remote machine user's environment.";
 }
