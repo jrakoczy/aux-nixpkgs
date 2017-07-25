d3ad925d8d2446bc7e79e0a4c42689d7b5301f38
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-07-25 17:54:16 +0200

    Fix syntactic error in desktop-environment.nix

--- pkgs/desktop-environment.nix
+++ pkgs/desktop-environment.nix
@@ -12,5 +12,5 @@ import ./mkEnvironment.nix {
   sha256 = "402e9ce4815330d6c731b937315d986e";
   rev = "159e0a7c62729270f7e1aeb9565873473a2712a2";
 
-  description = "A local machine user's environment."
+  description = "A local machine user's environment.";
 }
