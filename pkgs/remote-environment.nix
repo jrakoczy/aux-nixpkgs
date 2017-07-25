0351e7f0afe0135f60a47b71dfadb6a86f4900c5
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-07-25 19:24:01 +0200

    Update remote-env pkg

--- pkgs/remote-environment.nix
+++ pkgs/remote-environment.nix
@@ -8,8 +8,8 @@ import ./mkEnvironment.nix {
 
   ];
 
-  sha256 = "0d9q7459r1y9hs3w1gg24ilsjnzry88f4m5npwvxcy8i066nrp54";
-  rev = "ab8e5d3ff082060a8c42003c16384345fa9e9273";
+  sha256 = "1bdbyjnsnkxfs85d7l7b41ncpnhglz6nasg94dzq0ydpnxaqfhiw";
+  rev = "026d0b70960ea31516c18b482cc8a2be016225f7";
 
   description = "A remote machine user's environment.";
 }
