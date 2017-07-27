a7b49ba474adfe7a6d99987baa7664704de13f80
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-07-27 16:07:15 +0200

    Add envutils pkg

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ let
   callPackage = lib.callPackage;
 in
 {
+  envutils = callPackage ./pkgs/envutils.nix { };
   desktop-environment = callPackage ./pkgs/desktop-environment.nix { };
   remote-environment = callPackage ./pkgs/remote-environment.nix { };
 }
---
+++ pkgs/envutils.nix
@@ -0,0 +1,34 @@
+{ pkgs, stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  name = "envutils";
+
+  src = fetchFromGitHub {
+    owner = "jrakoczy";
+    repo = "envutils";
+    sha256 = "";
+    rev = "";
+  };
+
+  phases = [ "unpackPhase" "installPhase" ];
+
+  installPhase = ''
+    target="$out"/bin
+    mkdir -p "$target"
+    install -m 755 * "$target"
+  '';
+
+  meta = {
+    description = "Convenience scripts to manipulate env config files.";
+    longDescription = ''
+      The scripts allow to conveniently load to user's home directory, test
+      and clean up configuration files (mainly dot-files) stored in the
+      Nix store.
+
+      envload — load config files from the Nix store to user's home dir.
+      envtest — conveniently sym-link any file to user's home dir.
+      envdel - delete the previously loaded environment.
+    '';
+    license = stdenv.lib.licenses.gpl3Plus;
+  }
+}
