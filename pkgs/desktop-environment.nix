6516dc77be5869edbbbee8ace77b173680ef4627
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-09-15 15:15:45 +0200

    Update desktop-environemnt pkg

--- pkgs/desktop-environment.nix
+++ pkgs/desktop-environment.nix
@@ -9,8 +9,8 @@ import ../lib/mkEnvironment.nix {
     mpv
   ];
 
-  sha256 = "0zwwr9bffb2xf5fp06l8gg1frfh3j6svyam3339a6gi3iqfx7jbk";
-  rev = "5ef93be7473d381f3ddf5b9f3767c52c651db1a4";
+  sha256 = "14v4dswk4lzb60fgl2xqi8vf3ivcl4wm3lf2czr6z69s786n32vw";
+  rev = "bd9a0102baf598f7303b43f48fb3f1282ad7f56b";
 
   description = "A local machine user's environment.";
 }
